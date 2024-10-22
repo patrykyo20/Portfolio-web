c15434b2c646d5c30b784250d176275b623f99b9
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-10-22 14:47:57 +0200

    feat: upadate handlers prototypes

--- src/scripts/extensions/handlers.js
+++ src/scripts/extensions/handlers.js
@@ -13,3 +13,67 @@ HTMLElement.prototype.input = function (callback) {
     console.warn("Callback isn't function");
   }
 };
+
+HTMLElement.prototype.dragStart = function (callback) {
+  if (typeof callback == 'function') {
+    this.addEventListener('dragstart', callback);
+  } else {
+    console.warn("Callback isn't function")
+  }
+}
+
+HTMLElement.prototype.touchStart = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("touchstart", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
+
+HTMLElement.prototype.touchEnd = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("touchend", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
+
+HTMLElement.prototype.touchMove = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("touchmove", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
+
+HTMLElement.prototype.mouseDown = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("mousedown", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
+
+HTMLElement.prototype.mouseUp = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("mouseup", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
+
+HTMLElement.prototype.mouseLeave = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("mouseleave", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
+
+HTMLElement.prototype.mouseMove = function (callback) {
+  if (typeof callback == "function") {
+    this.addEventListener("mousemove", callback);
+  } else {
+    console.warn("Callback isn't function");
+  }
+};
