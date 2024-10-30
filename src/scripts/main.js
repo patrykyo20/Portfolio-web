ec971debf84de906bbf7dff0fb165b7c14ab51f7
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-10-30 11:33:25 +0100

    feat: implement components and functions to main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,20 @@
-import Project from "./components/Project.js";
 import "./extensions/handlers.js";
 import "./extensions/classNames.js";
+import Project from "./components/Project.js";
+import Experience from "./components/Experience.js";
+import Skills from "./components/Skills.js";
 import { openHamburger } from "./features/hamburger.js";
+import { showHeader } from "./features/showHeader.js";
+import { Form } from "./features/form.js";
 
 function main() {
   Project();
+  Experience();
+  Skills();
+
   openHamburger();
+  showHeader();
+  Form();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
